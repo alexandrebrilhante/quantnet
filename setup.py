9abf65d0cacd6feb91b6f7788d1f132db49a7d96
Alexandre Brilhante <alexandre.brilhante@gmail.com>
2024-07-19 09:07:45 -0400

    added deps

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-setuptools.setup(
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
     name="quantnet",
     version="0.1.0",
     author="Alexandre Brilhante",
@@ -20,6 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=requirements,
     python_requires=">=3.10",
 )
